0da5b3f62af7c45866836dc7e6db48eb7291d6bb
WarriorHanamy <632029496@qq.com>
2026-04-10 20:43:43 +0800

    clean the repo.

--- run_scripts/run-jetson-prod-lidar-discovery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-# Discover devices on enP8p1s0 interface
-# Returns all devices with valid MAC addresses
-
-INTERFACE="enP8p1s0"  # see the orin nx device
-
-ip neigh show dev "$INTERFACE" | grep -v "INCOMPLETE" | grep -v "FAILED" | awk '{print $1}' | grep -v "\.255$"
